5243bcf971cbb6be08d4ce7631f3e5b222f93a48
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-26 07:38:53 +1000

    🎉 Update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,34 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({ env }) => {
+  const imageHost = env("AWS_IMAGE_LOCATION", "").split("/")[2];
+
+  return [
+    "strapi::errors",
+    // "strapi::security",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          directives: {
+            "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
+            "img-src": [
+              "'self'",
+              "data:",
+              "cdn.jsdelivr.net",
+              "strapi.io",
+              "dl.airtable.com",
+              imageHost,
+            ],
+          },
+        },
+      },
+    },
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
